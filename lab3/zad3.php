69a9f647c227e6e6f8b8102f8b5df060bafc4147
mikeshaggy <michalbagan@icloud.com>
2024-05-12 16:42:08 +0200

    Lab 3

--- lab3/zad3.php
+++ lab3/zad3.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Directory Management</title>
+    <title>Zadanie 3</title>
 </head>
 <body>
 <form action="zad3result.php" method="post">
